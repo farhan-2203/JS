7a0e635eeef4bda63634116c3e26773f59f4a0f5
farhan-2203 <farhan.cscf@gmail.com>
2025-03-04 22:42:13 +0500

    Cond_Practice

---
+++ JS_CondStatement_01.js
@@ -0,0 +1,8 @@
+/* Qs1. Get user to input a number using prompt(“Enter a number:”). 
+Check if the number is a multiple of 5 or not.*/
+let num=prompt("Enter a Number");
+if (num%5===0) {
+    console.log(num+" is a Multiple of 5");
+} else {
+    console.log(num+" is not a Multiple of 5");
+}
