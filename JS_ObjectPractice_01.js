331be11f80828971b992d15edde2480487316133
farhan-2203 <farhan.cscf@gmail.com>
2025-03-04 21:09:10 +0500

    Create_OBJ

---
+++ JS_ObjectPractice_01.js
@@ -0,0 +1,8 @@
+// Qs1. Create a const object called “product” to store information shown in the picture.
+const product={
+    productName:"Parker Jotter Standard Ball pen",
+    color:"black",
+    isDeal:true,
+    price:270,
+    Sale:"5% off"
+}
---
+++ JS_ObjectPractice_02.js
@@ -0,0 +1,9 @@
+// Qs2. Create a const object called “profile” to store information shown in the picture.
+const profile={
+    name:"ShradhaKhapra",
+    isFollow:true,
+    totalPosts:195,
+    followers:569000,
+    following:4,
+    userName:"@shradhakhapra"
+}
